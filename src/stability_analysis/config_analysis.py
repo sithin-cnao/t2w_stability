8bd3c411b8449eb196edcdbcd8cc85eb111de733
Sithin Thulasi Seetha <sithinthulasiseetha@gmail.com>
2026-07-31 09:39:14 +0200

    config sensititivy analysis completed

--- src/stability_analysis/config_analysis.py
+++ src/stability_analysis/config_analysis.py
@@ -33,6 +33,7 @@ def estimator(fs_df, nfs_df):
     ccc_df = compute_ccc(fs_df, nfs_df)
     return ccc_df.ccc.median()
 
+#%%
 if __name__=="__main__":
 
     observed_median_ccc = {}
@@ -96,104 +97,105 @@ if __name__=="__main__":
             
     np.savez_compressed(os.path.join(OUT_DIR, "bootstrap_config_results.npz"), bootstrap_median_ccc = bootstrap_median_ccc, observed_median_ccc = observed_median_ccc, observed_ccc_dfs = observed_ccc_dfs, bootstrap_ccc_dfs = bootstrap_ccc_dfs)
 
-   
-    # analyze results
-    bootstrap_config_results = np.load(os.path.join(OUT_DIR, "bootstrap_config_results.npz"), allow_pickle=True)
-    bootstrap_median_ccc = bootstrap_config_results['bootstrap_median_ccc'].item()
-    observed_median_ccc = bootstrap_config_results['observed_median_ccc'].item()
+#%%
+# analyze results
+bootstrap_config_results = np.load(os.path.join(OUT_DIR, "bootstrap_config_results.npz"), allow_pickle=True)
+bootstrap_median_ccc = bootstrap_config_results['bootstrap_median_ccc'].item()
+observed_median_ccc = bootstrap_config_results['observed_median_ccc'].item()
 
-    bootstrap_ccc_dfs = bootstrap_config_results['bootstrap_ccc_dfs'].item()
-    observed_ccc_dfs = bootstrap_config_results['observed_ccc_dfs'].item()
+bootstrap_ccc_dfs = bootstrap_config_results['bootstrap_ccc_dfs'].item()
+observed_ccc_dfs = bootstrap_config_results['observed_ccc_dfs'].item()
 
-    configs = list(itertools.product(BIAS_CORRECTION, ZNORM_ROI, ["normal", "combat"]))
-    bootstrap_matrix = np.array([bootstrap_median_ccc[config] for config in configs])
-    true_matrix = np.array([observed_median_ccc[config] for config in configs])
+configs = list(itertools.product(BIAS_CORRECTION, ZNORM_ROI, ["normal", "combat"]))
+bootstrap_matrix = np.array([bootstrap_median_ccc[config] for config in configs])
+true_matrix = np.array([observed_median_ccc[config] for config in configs])
 
-    best_config = None
-    max_value = -np.inf
+best_config = None
+max_value = -np.inf
 
-    config_stats = {"config": [], "observed_median_ccc": [], "bootstrap_median_ccc": [], "bias": [], "95% CI":[], "95% BCa CI": []}
-    for bc in BIAS_CORRECTION:
-        for roi in ZNORM_ROI:
+config_stats = {"config": [], "observed_median_ccc": [], "bootstrap_median_ccc": [], "bias": [], "95% CI":[], "95% BCa CI": []}
+for bc in BIAS_CORRECTION:
+    for roi in ZNORM_ROI:
 
-            sub_df = radiomics_df[(radiomics_df.bias_correction == bc) & (radiomics_df.norm_roi == roi) & (radiomics_df.tissue == TISSUE)].reset_index(drop=True)
-            combat_sub_df = sub_df.copy()
-            combat_sub_df[features] = combat_correction(sub_df[features], sub_df[["sequence"]], batch_col="sequence")
-            
-            fs_df = sub_df[sub_df.sequence=="t2w_fs"].sort_values(by="id").reset_index(drop=True)
-            nfs_df = sub_df[sub_df.sequence=="t2w_nfs"].sort_values(by="id").reset_index(drop=True)
-            
-            combat_fs_df = combat_sub_df[sub_df.sequence=="t2w_fs"].sort_values(by="id").reset_index(drop=True)
-            combat_nfs_df = combat_sub_df[sub_df.sequence=="t2w_nfs"].sort_values(by="id").reset_index(drop=True)
-
-            for data_type, dfs in {"normal":(fs_df, nfs_df), "combat":(combat_fs_df, combat_nfs_df)}.items():
-                df1, df2 = dfs
-                print("config=", (bc, roi, data_type))
-                estimate = observed_median_ccc[(bc, roi, data_type)]
-                bootstrap_estimates = bootstrap_median_ccc[(bc, roi, data_type)]
-                
-                bootstrap_mean = np.mean(bootstrap_estimates)
-                bootstrap_ci = np.quantile(bootstrap_estimates, [0.025, 0.975])
-                bias = bootstrap_mean - estimate
-                
-                corr_ci = bca_bootstrap_ci(estimator, df1[features], df2[features], estimate, bootstrap_estimates)
-            
-                
-                print(f"Sample Estimate: {estimate:.3f}")
-                print(f"Bootstrap Mean: {bootstrap_mean:.3f}")
-                print(f"95% CI: {bootstrap_ci}")
-                print(f"Bias: {bias:.3f}")
-                print(f"95% BCa CI: {corr_ci}")
-
-                config_stats["config"].append((bc, roi, data_type))
-                config_stats["observed_median_ccc"].append(estimate)
-                config_stats["bootstrap_median_ccc"].append(bootstrap_mean)
-                config_stats["bias"].append(bias)
-                config_stats["95% CI"].append(bootstrap_ci)
-                config_stats["95% BCa CI"].append(corr_ci)
-
-                if estimate>max_value:
-                    max_value = estimate
-                    best_config = (bc, roi, data_type)
-
-
-    config_stats_df = pd.DataFrame(config_stats)
-    display(config_stats_df)
-    config_stats_df.to_csv(os.path.join(OUT_DIR, "config_stats.csv"), index=False)
-
-    #%%
-    config_pvalues = {"config":[], "p-value":[]}
-    for config in configs:
-
-        if config == best_config:
-            continue
-
-        best_config_df = observed_ccc_dfs[best_config].sort_values(by="feature").reset_index(drop=True)
-        config_df = observed_ccc_dfs[config].sort_values(by="feature").reset_index(drop=True)
+        sub_df = radiomics_df[(radiomics_df.bias_correction == bc) & (radiomics_df.norm_roi == roi) & (radiomics_df.tissue == TISSUE)].reset_index(drop=True)
+        combat_sub_df = sub_df.copy()
+        combat_sub_df[features] = combat_correction(sub_df[features], sub_df[["sequence"]], batch_col="sequence")
         
-        statistic, p_value = mannwhitneyu(config_df.ccc, best_config_df.ccc)
-        print(f"For config: {config}, The p-value from mann-whitney u test with best_config: {best_config} is {p_value}")
+        fs_df = sub_df[sub_df.sequence=="t2w_fs"].sort_values(by="id").reset_index(drop=True)
+        nfs_df = sub_df[sub_df.sequence=="t2w_nfs"].sort_values(by="id").reset_index(drop=True)
+        
+        combat_fs_df = combat_sub_df[sub_df.sequence=="t2w_fs"].sort_values(by="id").reset_index(drop=True)
+        combat_nfs_df = combat_sub_df[sub_df.sequence=="t2w_nfs"].sort_values(by="id").reset_index(drop=True)
 
-        config_pvalues['config'].append(config)
-        config_pvalues['p-value'].append(p_value)
+        for data_type, dfs in {"normal":(fs_df, nfs_df), "combat":(combat_fs_df, combat_nfs_df)}.items():
+            df1, df2 = dfs
+            print("config=", (bc, roi, data_type))
+            estimate = observed_median_ccc[(bc, roi, data_type)]
+            bootstrap_estimates = bootstrap_median_ccc[(bc, roi, data_type)]
+            
+            bootstrap_mean = np.mean(bootstrap_estimates)
+            bootstrap_ci = np.quantile(bootstrap_estimates, [0.025, 0.975])
+            bias = bootstrap_mean - estimate
+            
+            corr_ci = bca_bootstrap_ci(estimator, df1[features], df2[features], estimate, bootstrap_estimates)
+        
+            
+            print(f"Sample Estimate: {estimate:.3f}")
+            print(f"Bootstrap Mean: {bootstrap_mean:.3f}")
+            print(f"95% CI: {bootstrap_ci}")
+            print(f"Bias: {bias:.3f}")
+            print(f"95% BCa CI: {corr_ci}")
 
-    config_pvalues_df = pd.DataFrame(config_pvalues)
+            config_stats["config"].append((bc, roi, data_type))
+            config_stats["observed_median_ccc"].append(estimate)
+            config_stats["bootstrap_median_ccc"].append(bootstrap_mean)
+            config_stats["bias"].append(bias)
+            config_stats["95% CI"].append(bootstrap_ci)
+            config_stats["95% BCa CI"].append(corr_ci)
 
-    _, bonf_pvals_corrected, _, _ = multipletests(
-        config_pvalues_df['p-value'], 
-        alpha=0.05, 
-        method='bonferroni'
-    )
-    config_pvalues_df['bonf_corrected_p-value'] = bonf_pvals_corrected
+            if estimate>max_value:
+                max_value = estimate
+                best_config = (bc, roi, data_type)
 
-    _, bh_pvals_corrected, _, _ = multipletests(
-        config_pvalues_df['p-value'], 
-        alpha=0.05, 
-        method='fdr_bh'
-    )
-    config_pvalues_df['bh_corrected_p-value'] = bh_pvals_corrected
 
-    config_pvalues_df.to_csv(os.path.join(OUT_DIR, "config_pvalues.csv"), index=False)
-    display(config_pvalues_df)
+config_stats_df = pd.DataFrame(config_stats)
+print(config_stats_df)
+config_stats_df.to_csv(os.path.join(OUT_DIR, "config_stats.csv"), index=False)
+
+
+#%%
+config_pvalues = {"config":[], "p-value":[]}
+for config in configs:
+
+    if config == best_config:
+        continue
+
+    best_config_df = observed_ccc_dfs[best_config].sort_values(by="feature").reset_index(drop=True)
+    config_df = observed_ccc_dfs[config].sort_values(by="feature").reset_index(drop=True)
+    
+    statistic, p_value = mannwhitneyu(config_df.ccc, best_config_df.ccc)
+    print(f"For config: {config}, The p-value from mann-whitney u test with best_config: {best_config} is {p_value}")
+
+    config_pvalues['config'].append(config)
+    config_pvalues['p-value'].append(p_value)
+
+config_pvalues_df = pd.DataFrame(config_pvalues)
+
+_, bonf_pvals_corrected, _, _ = multipletests(
+    config_pvalues_df['p-value'], 
+    alpha=0.05, 
+    method='bonferroni'
+)
+config_pvalues_df['bonf_corrected_p-value'] = bonf_pvals_corrected
+
+_, bh_pvals_corrected, _, _ = multipletests(
+    config_pvalues_df['p-value'], 
+    alpha=0.05, 
+    method='fdr_bh'
+)
+config_pvalues_df['bh_corrected_p-value'] = bh_pvals_corrected
+
+config_pvalues_df.to_csv(os.path.join(OUT_DIR, "config_pvalues.csv"), index=False)
+print(config_pvalues_df)
 
                 
